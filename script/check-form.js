4d9791a46032141dff1a397795394fa3e466c1a0
jinnnii <jinnnii@naver.com>
2022-01-13 18:51:48 +0900

    Feat : Checking input boxs for sign up

--- script/check-form.js
+++ script/check-form.js
@@ -13,23 +13,57 @@ const birth = document.frm.birth;
 const sex = document.frm.sex;
     
 
-document.querySelector('.join-btn').addEventListener('click', (event) => {
-
-    const user = {
-        id: id,
-        pass: pass,
-        passComfirm: passComfirm,
-        firstNum : firstNum,
-        secondNum:secondNum,
-        thirdNum:thirdNum,
-        confirmNum:confirmNum
-    }
-    Object.values(user).forEach((element) => {
-        if (element.value == "") {
-            // console.log(element.nextSibling.nextElementSibling.innerHTML);
-            console.log(element)
+document.querySelector('.join-btn').addEventListener('click',(event) => {
+    const user=[
+        id,pass,passComfirm,firstNum, secondNum, thirdNum, confirmNum
+    ]
+    for(let i=0; i<user.length; i++){
+        const value = checkValue(user[i]);
+        console.log(value);
+        if(value){
+            let name;
+            if(value.name=='id'){
+                name='아이디';
+            }
+            if(value.name=='pass'|| value.name=="pass-cf"){
+                name='비밀번호';
+            }
+            if(value.name=='first-num'
+                ||value.name=='second-num'
+                || value.name=='third-num'){
+                name='휴대폰 번호'
+            }
+            if(value.name=='code-cf'){
+                name='인증번호'
+            }
+            alertMessage(name);
+            value.focus();
+            event.preventDefault();
+            break;
         }
-    })
-    event.preventDefault();
+    }
+    if (pass.value.length>0){
+        if( pass.value.length<8||pass.value.length>16){
+            alert('비밀번호는 8자리 이상 16자리 이하로 설정해주세요');
+            pass.focus();
+            event.preventDefault();
+        }
+        if( pass.value!=passComfirm.value){
+            alert('비밀번호가 일치하지 않습니다')
+            passComfirm.focus();
+            event.preventDefault();
+        }
+    }
 
-})
+});
+
+function checkValue(element){
+    if(element.value==""){
+        return element;
+    }
+    return;
+}
+
+function alertMessage(name){
+    alert(`${name}를 확인해 주세요.`);
+}
